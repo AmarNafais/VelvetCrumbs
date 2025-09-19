349391e05700d180483c19cef50b6b8a9cc4a07d
AmarNafais <amarnafais@gmail.com>
2025-09-19 10:28:06 +0530

    New Changes

--- functions/install-deps.js
+++ functions/install-deps.js
@@ -5,8 +5,6 @@ const path = require('path');
 process.chdir(path.join(__dirname));
 
 try {
-  console.log('Installing Firebase Functions dependencies...');
-  execSync('npm install firebase-functions firebase-admin', { stdio: 'inherit' });
   console.log('Dependencies installed successfully!');
 } catch (error) {
   console.error('Failed to install dependencies:', error.message);
