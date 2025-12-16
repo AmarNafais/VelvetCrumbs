585ee74b21938b0772c61aa506b2e1dc0145fdcd
NabeelMohideen <nabeelnabeela8@gmail.com>
2025-12-16 13:23:52 +0530

    cleaned files

--- functions/src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import app from "./server";
-
-// Initialize Firebase Admin
-admin.initializeApp();
-
-// Export the Express app as a Firebase function
-export const api = functions.https.onRequest(app);
